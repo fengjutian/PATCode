5e8dba314631a8d84f0ccf20c7da78d6234343c6
fengjutian <2640111081@qq.com>
2020-09-07 20:21:13 +0800

    add

--- B1032.cpp
+++ B1032.cpp
@@ -6,3 +6,26 @@
 输出格式：
 在一行中给出得分最高的学校的编号及其总分，中间以空格分隔。题目答案唯一，没有并列。
 */
+
+#include <cstdio>
+const int maxn = 100010;
+int school[maxn] = {0};
+int main() {
+    int n, schID, score;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d%d", &schID, &score); // 学校ID,分数
+        school[schID] += score; // 学校schID的总分增加score
+    }
+
+    int k=1, MAX=-1; // 最高总分数的学校ID以及总分数
+    for (int i = 1; i <= n; i++) {
+        if(school[i] > MAX) {
+            MAX = school[i];
+            k = i;
+        }
+    }
+
+    printf("%d %d\n",k, MAX); // 输出最高总分的学校ID及其总分
+    return 0;
+}
